85461d3a42bbb7d49f6647e0d784b3bf1b6e785e
Udhaya kumar <udhaya642003@gmail.com>
2024-03-27 09:02:14 +0530

    second commit

--- glaucoma_app.py
+++ glaucoma_app.py
@@ -41,7 +41,26 @@ else:
         st.balloons()
     else:
         st.write("""
-                 ## **Prediction:** You are affected by Glaucoma. Please consult an ophthalmologist as soon as possible.
-                 ""","Amount of prediction%: ",pred
+                 # Prediction: You are affected by Glaucoma. Please consult an ophthalmologist as soon as possible.
+                 """
                  )
+        st.write(""" 
+                 ## **Amount of prediction %:**""",pred)
+        st.write(""" ### Here are some symptoms of glaucoma that users can watch out for: 
+
+##### Gradual Loss of Peripheral Vision: One of the earliest signs of glaucoma is often the gradual loss of peripheral (side) vision. This may go unnoticed at first but can progress over time if left untreated.
+
+##### Blurred Vision: Individuals with glaucoma may experience blurred or hazy vision, especially in low light conditions.
+
+##### Halos Around Lights: Glaucoma can cause halos or rainbow-colored rings to appear around lights, particularly at night.
+
+##### Intense Eye Pain: In acute cases of angle-closure glaucoma, individuals may experience sudden, severe eye pain, along with headaches and nausea.
+
+##### Redness in the Eye: Glaucoma can cause the eye to appear red or bloodshot due to increased pressure within the eye.
+
+##### Nausea or Vomiting: In acute angle-closure glaucoma, individuals may experience nausea or vomiting along with severe eye pain.
+
+##### Vision Loss: As glaucoma progresses, it can lead to permanent vision loss or blindness if left untreated. This typically starts with peripheral vision loss and can advance to tunnel vision or complete blindness.
+
+It's essential to note that early-stage glaucoma may not present any noticeable symptoms, which is why regular eye exams, especially for individuals over 40 or with a family history of glaucoma, are critical for early detection and treatment. If users experience any of the above symptoms, they should seek immediate medical attention from an eye care professional for a comprehensive eye examination. Early diagnosis and treatment can help slow or prevent further vision loss caused by glaucoma.  """)
 
